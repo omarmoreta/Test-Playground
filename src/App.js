32ea0df2cf03e7d36294d6f3d3858add732e183d
Omar <Omar>
2022-02-10 23:31:16 -0500

    test for the checkbox

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 function App() {
   const [btnColor, setBtnColor] = useState("green");
   const newBtnColor = btnColor === "green" ? "blue" : "green";
+  const [inputDisabled, setInputDisabled] = useState(false);
 
   return (
     <div className="App">
@@ -12,9 +13,16 @@ function App() {
       <button
         style={{ backgroundColor: btnColor }}
         onClick={() => setBtnColor(newBtnColor)}
+        disabled={inputDisabled}
       >
         Change button color to {newBtnColor}
       </button>
+
+      <input
+        type="checkbox"
+        defaultChecked={inputDisabled}
+        onChange={(event) => setInputDisabled(event.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -22,3 +22,15 @@ describe("test for the button", () => {
     expect(colorBtn.textContent).toBe("Change button color to green");
   });
 });
+
+test("checkbox disables button when first clicked, then enables button on second click", () => {
+  render(<App />);
+  const colorBtn = screen.getByRole("button");
+  const checkbox = screen.getByRole("checkbox");
+
+  userEvent.click(checkbox);
+  expect(colorBtn).toBeDisabled();
+
+  userEvent.click(checkbox);
+  expect(checkbox).toBeEnabled;
+});
