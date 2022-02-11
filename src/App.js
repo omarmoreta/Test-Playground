ef52d7e8b722179df3895dcc23e43561d339c290
Omar <Omar>
2022-02-10 23:19:28 -0500

    test for the button clicked to different color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [btnColor, setBtnColor] = useState("green");
+  const newBtnColor = btnColor === "green" ? "blue" : "green";
+
   return (
     <div className="App">
       <h1>Testing Playground</h1>
+      <button
+        style={{ backgroundColor: btnColor }}
+        onClick={() => setBtnColor(newBtnColor)}
+      >
+        Change button color to {newBtnColor}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("header renders with correct text", () => {
   //Create a virual DOM of desired component
@@ -8,4 +9,16 @@ test("header renders with correct text", () => {
   const headerEl = screen.getByRole("heading");
   //Assertion to look for the element in the virtual DOM using toBe matcher to test exact equality
   expect(headerEl.textContent).toBe("Testing Playground");
+});
+
+describe("test for the button", () => {
+  test("button changes color when clicked", () => {
+    render(<App />);
+    const colorBtn = screen.getByRole("button");
+
+    userEvent.click(colorBtn);
+
+    expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+    expect(colorBtn.textContent).toBe("Change button color to green");
+  });
 });
